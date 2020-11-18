0e96c1f9f52e183638c82ae979a6c731ba73877e
JGE <Hell0@schmalz>
2020-11-18 12:01:40 +0100

    sreen update

--- release/bin/update-screen.sh
+++ release/bin/update-screen.sh
@@ -7,5 +7,5 @@ echo "base_dir: $base_dir"
 echo "pic_dir: $pic_dir"
 
 xvfb-run --server-args="-screen 0, 800x480x24" cutycapt --url=$1 --out=$pic_dir/screen.png
-convert $pic_dir/screen.png -crop 800x480 -colorspace gray -depth 2 -t                    ype truecolor $pic_dir/screen.bmp
+convert $pic_dir/screen.png -crop 800x480 -colorspace gray -depth 2 -type truecolor $pic_dir/screen.bmp
 python3 $base_dir/waveshare/py/updatescreen.py
