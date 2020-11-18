7712890f3711dbb050a118b8869f6c45f48c7a32
JGE <Hell0@schmalz>
2020-11-18 12:46:21 +0100

    sreen update

--- release/bin/update-screen.sh
+++ release/bin/update-screen.sh
@@ -2,10 +2,6 @@
 base_dir=$(dirname $(dirname $(realpath $0)) )
 pic_dir="$base_dir/waveshare/pic"
 
-echo "source: $1"
-echo "base_dir: $base_dir"
-echo "pic_dir: $pic_dir"
-
 xvfb-run --server-args="-screen 0, 480x800x24" cutycapt --url=$1 --out=$pic_dir/screen.png
-convert $pic_dir/screen.png -rotate 90 -colorspace gray -depth 2 -type truecolor $pic_dir/screen.bmp
+convert $pic_dir/screen.png -rotate 90 -gravity center -crop 800x480+0+0 -threshold 90% -monochrome $pic_dir/screen.bmp
 python3 $base_dir/waveshare/py/updatescreen.py
