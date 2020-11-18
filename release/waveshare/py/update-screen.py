8f53a4263b22a747956e7f6719266634f6880c9d
JGE <Hell0@schmalz>
2020-11-18 11:12:00 +0100

    add update support

---
+++ release/waveshare/py/update-screen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    epd = epd7in5_V2.EPD()
+    epd.init()
+
+    logging.info("read bmp file")
+    Himage = Image.open(os.path.join(picdir, 'screen.bmp'))
+    epd.display(epd.getbuffer(Himage))
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    epd.Dev_exit()
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd7in5.epdconfig.module_exit()
+    exit()
